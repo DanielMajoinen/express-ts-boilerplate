55d51549c78ea3a064511b42597172e5155e5644
daniel <daniel@majoinen.com>
2021-10-05 13:18:39 +1100

    add hot reload debugging

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express'
+import pino from 'pino'
+import pinoHttp from 'pino-http'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+const logger = pino()
+pinoHttp({ logger })
+app.use(pino)
 
 app.get('/', (req, res) => {
   res.send('Express')
 })
 
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
+  logger.info(`Listening on http://localhost:${port}`)
 })
